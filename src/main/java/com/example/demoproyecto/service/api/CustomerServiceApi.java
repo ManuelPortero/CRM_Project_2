3a83455c003eb992ff66768081b898461d398181
Eva Clariana <eva.clariana@gmail.com>
2020-06-03 20:18:34 +0200

    no message

--- src/main/java/com/example/demoproyecto/service/api/CustomerServiceApi.java
+++ src/main/java/com/example/demoproyecto/service/api/CustomerServiceApi.java
@@ -2,7 +2,7 @@ package com.example.demoproyecto.service.api;
 import com.example.demoproyecto.commons.GenericServiceAPI;
 import com.example.demoproyecto.model.Customer;
 
-public interface CustomerServiceApi extends GenericServiceAPI <Customer, Long> {
+public interface CustomerServiceAPI extends GenericServiceAPI <Customer, Long> {
    
 }
 
