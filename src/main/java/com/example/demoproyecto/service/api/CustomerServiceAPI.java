115364a084706241bcd8c17be85bc126fdf5bc07
Eva Clariana <eva.clariana@gmail.com>
2020-06-04 19:36:05 +0200

    no message

--- src/main/java/com/example/demoproyecto/service/api/CustomerServiceAPI.java
+++
@@ -1,8 +0,0 @@
-package com.example.demoproyecto.service.api;
-import com.example.demoproyecto.commons.GenericServiceAPI;
-import com.example.demoproyecto.model.Customer;
-
-public interface CustomerServiceAPI extends GenericServiceAPI <Customer, Long> {
-   
-}
-
