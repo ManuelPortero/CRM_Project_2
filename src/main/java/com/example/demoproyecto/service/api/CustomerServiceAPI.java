862fe2323e5f0be9f61f080688cf3973ed861536
Eva Clariana <eva.clariana@gmail.com>
2020-06-06 20:24:16 +0200

    Revert "no message"
    This reverts commit 115364a084706241bcd8c17be85bc126fdf5bc07.

---
+++ src/main/java/com/example/demoproyecto/service/api/CustomerServiceAPI.java
@@ -0,0 +1,8 @@
+package com.example.demoproyecto.service.api;
+import com.example.demoproyecto.commons.GenericServiceAPI;
+import com.example.demoproyecto.model.Customer;
+
+public interface CustomerServiceAPI extends GenericServiceAPI <Customer, Long> {
+   
+}
+
